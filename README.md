97f8e429827f06d4de0d3e7f2a681dd1b241ace6
Justin Nunez <justinnunez1717@gmail.com>
2019-10-15 20:54:44 +0000

    Added list of recipients; Performed quality of life updates/general cleanup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PizzaDScraper
 Web scraping tool to find an notify entities about the greatest pizza in NOLA
+
+Included files:
+  index.js
+    - Scraping logic
+  recipients
+    - List of message recipients with country and area code (i.e. +1001XXX5551234)
+
+Important Note:
+The aws sdk will not function without first establishing AWS credentials
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const AWS = require("aws-sdk");
+const fs = require("fs");
 AWS.config.update({region: 'us-east-1'});
 
 const url = "http://pizzadelicious.com/";
 
+
+//-------------------------- Helper Functions --------------------------------
+
+// Function to fetch data from url
 const fetchData = async (url) => {
   const result = await axios.get(url);
   return cheerio.load(result.data);
 };
 
+// Function to scrape the data for list of current pizzas available
 const fetchPizzaList = async (url) => {
   const $ = await fetchData(url);
   let pizzas;
@@ -21,41 +27,46 @@ const fetchPizzaList = async (url) => {
   return pizzas;
 }
 
+// Function to send alert to given recipients
+const sendEggplantAlert = (recipients) => {
+  let recipientsByLine = recipients.split("\n");
+  console.log("Recipients:\n" + recipientsByLine);
+
+  // Send alert to each recipient
+  for (let i = 0; i < (recipientsByLine.length - 1); ++i) {
+    let params = { Message: "*PIZZA DELICIOUS ALERT*\nEggplant Parmigiana Pizza Available Today"};
+    params.PhoneNumber = recipientsByLine[i];
+
+    let publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+    publishTextPromise.then(
+      function(data) {
+        console.log("Message \"" + params.Message +"\" sent to " + params.PhoneNumber);
+        console.log("MessageID is " + data.MessageId);
+      }).catch(
+        function(err) {
+        console.error(err, err.stack);
+      });
+  }
+}
+
+//----------------------------------------------------------------------------
 
 fetchPizzaList(url).then((data) => {
   let pizzaList = String(data);
-  console.log("Today's Pizzas:\n" + pizzaList);
+  console.log("Today's Pizzas:\n" + pizzaList + '\n');
 
-  let params = {
-    Message: "*PIZZA DELICIOUS ALERT*\nEggplant Parmigiana Pizza Available Today", 
-    //TopicArn: 'arn:aws:sns:us-east-2:433850899585:pizza-d-scraper-recipients'
-    PhoneNumber: '+10015042391142',
-  };
-
+/*
+  // For testing purposes
   if(pizzaList.includes("Cheese")){
     console.log("Found Cheese");
-    let publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
- 
-    publishTextPromise.then(
-      function(data) {
-        console.log("Message \'" + params.Message + "\' sent to the topic \'" + params.TopicArn);
-        console.log("MessageID is " + data.MessageId);
-      }).catch(
-        function(err) {
-        console.error(err, err.stack);
-      });
+    let recipients = fs.readFileSync("recipients", "utf8");
+    sendEggplantAlert(recipients);
+  }
+*/
+  if(pizzaList.includes("Eggplant")){
+    console.log("Found Eggplant Parmigiana Pizza");
+    let recipients = fs.readFileSync("recipients", "utf8");
+    sendEggplantAlert(recipients);
   }
 
-  if(pizzaList.includes("Eggplant")){
-    let publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
- 
-    publishTextPromise.then(
-      function(data) {
-        console.log("Message \'" + params.Message + "\' sent to the topic \'" + params.TopicArn);
-        console.log("MessageID is " + data.MessageId);
-      }).catch(
-        function(err) {
-        console.error(err, err.stack);
-      });
-  }
 });
